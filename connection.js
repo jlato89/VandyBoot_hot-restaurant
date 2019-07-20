85fc4b7cab43c5c1b07ffc1a87ab300fc2be44ed
Kraus, Simon <simon.kraus@aol.com>
2019-07-20 10:37:34 -0500

    did all get paths

--- connection.js
+++ connection.js
@@ -14,6 +14,13 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
+app.get("/reserve", function(req,res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"))
+})
 
 
 
